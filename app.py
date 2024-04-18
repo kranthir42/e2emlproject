0a8e94dd180c178b170830e72a3d9c8ffc5da7b5
kranthi kiran <kranthir42@gmail.com>
2024-04-18 17:42:49 +0530

    data ingestion

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from src.e2emlproject.logger import logging
 from src.e2emlproject.exception import CustomException
+from src.e2emlproject.components.data_ingestion import DataIngestion, DataIngestionConfig
 import sys
 
 if __name__ == "__main__":
     logging.info("The execution has started")
     
     try:
-        a = 1/0
+        data_ingestion_config=DataIngestionConfig()
+        data_ingestion = DataIngestion()
+        train_data_path,test_data_path = data_ingestion.initiate_data_ingestion()
+        
     except Exception as e:
         logging.info("custom exception")
         raise CustomException(e, sys)
+   
